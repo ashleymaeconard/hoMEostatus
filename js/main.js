bcc33a46ffaaddc1b07223f84090c29048e11485
Ashley Mae Conard <ashley_conard@brown.edu>
2016-02-07 12:44:13 -0500

    changed axis height again

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ function drawHRGraph(HR){
           tick: {
                 rotate: 75
           },
-          height: 0
+          height: 10
           // tick: {
           //   //format: '%Y-%m-%d'
           //   format: function(x) { 
@@ -148,7 +148,7 @@ function drawSkinTempGraph(SkinTemp){
           tick: {
             rotate: 75
           },
-          height: 130
+          height: 10
           // tick: {
           //   //format: '%Y-%m-%d'
           //   format: function(x) { 
@@ -197,7 +197,7 @@ function drawMotionGraph(Motion){
           tick: {
                 rotate: 75
           },
-          height: 130
+          height: 10
         }
       }
       });
